49e319030eb6770b1547f334395872a30e35da16
MG <85082249+matthias-grgic@users.noreply.github.com>
2022-03-01 16:56:12 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Hello() {
 
 const StyledLink = styled.div`
   background-color: transparent;
-  font-size: clamp(3.5rem, 3vw, 3rem);
+  font-size: clamp(3rem, 3vw, 3.5rem);
   z-index: 1;
   @media (min-width: 810px) {
     padding: 0 16rem 0 16rem;
