f08264e844169be6ba1b336a47a8089f340761da
MG <matthias.grgic@gmail.com>
2022-03-05 14:31:13 +0100

    multiple bingos working, organized code

--- src/page/Bingo.jsx
+++ src/page/Bingo.jsx
@@ -3,24 +3,11 @@ import { shuffledArray } from '../lib/statements'
 import { useState, useEffect } from 'react'
 import LottieAnimation from '../components/LottieAnimation'
 import ButtonReseter from '../components/ButtonReset'
+import { allBingos } from '../lib/allBingos'
 
 export default function Bingo() {
   const [howManyBingos, setHowManyBingos] = useState(0)
   const [isClicked, setIsClicked] = useState([12])
-  const allBingos = [
-    [0, 1, 2, 3, 4],
-    [5, 6, 7, 8, 9],
-    [10, 11, 12, 13, 14],
-    [15, 16, 17, 18, 19],
-    [20, 21, 22, 23, 24],
-    [0, 5, 10, 15, 20],
-    [1, 6, 11, 16, 21],
-    [2, 7, 12, 17, 22],
-    [3, 8, 13, 18, 23],
-    [4, 9, 14, 19, 24],
-    [0, 6, 12, 18, 24],
-    [4, 8, 12, 16, 20],
-  ]
 
   //let the loop run only once if goal is achieved
   let [var1, setVar1] = useState(true)
